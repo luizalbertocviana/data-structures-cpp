e909720606f01936aa40eadc6f7c5bcc4b80e534
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-01 01:13:13 -0300

    begin to write some assert tests

--- avltree_test.cpp
+++ avltree_test.cpp
@@ -3,11 +3,27 @@
 #include <avltree.hpp>
 
 int main(){
-  AVLTree<int, std::string> avlt{1, "eita"};
+  AVLTree<int, std::string> avlt{};
 
-  AVLTree<int, std::string> avlt2{};
+  avlt.insert(1, "eita");
+  avlt.insert(2, "haha");
+  avlt.insert(3, "eita");
+  avlt.insert(4, "dorime");
+  avlt.insert(5, "ameno");
+
+  assert(*avlt.search(1) == "eita");
+  assert(*avlt.search(2) == "haha");
+  assert(*avlt.search(3) == "eita");
+  assert(*avlt.search(4) == "dorime");
+  assert(*avlt.search(5) == "ameno");
   
   avlt.remove(1);
   
+  assert(avlt.search(1) == std::nullopt);
+  assert(*avlt.search(2) == "haha");
+  assert(*avlt.search(3) == "eita");
+  assert(*avlt.search(4) == "dorime");
+  assert(*avlt.search(5) == "ameno");
+
   return 0;
 }
