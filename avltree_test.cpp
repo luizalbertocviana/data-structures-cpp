e417bd9878a326187468fb837e0a29fb7fa30b5e
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-05 08:56:07 -0300

    added more tests and a debug clause

--- avltree_test.cpp
+++ avltree_test.cpp
@@ -1,29 +1,74 @@
 #include <cassert>
 
+// uncomment this to see some logging messages
+// #define debug
+
+#ifdef debug
+#include <iostream>
+#endif
+
 #include <avltree.hpp>
 
-int main(){
+void test1(){
+  #ifdef debug
+  std::cout << "test 1" << std::endl;
+  #endif
+  AVLTree<int, std::string> avlt{};
+
+  avlt.insert(4, "eita");
+  avlt.insert(3, "haha");
+  avlt.insert(2, "hehe");
+  avlt.insert(1, "hihi");
+
+  assert(*avlt.search(4) == "eita");
+  assert(*avlt.search(3) == "haha");
+  assert(*avlt.search(2) == "hehe");
+  assert(*avlt.search(1) == "hihi");
+}
+
+void test2(){
+  #ifdef debug
+  std::cout << "test 2" << std::endl;
+  #endif
   AVLTree<int, std::string> avlt{};
 
   avlt.insert(1, "eita");
   avlt.insert(2, "haha");
-  avlt.insert(3, "eita");
-  avlt.insert(4, "dorime");
-  avlt.insert(5, "ameno");
+  avlt.insert(3, "hehe");
+  avlt.insert(4, "hihi");
 
+  assert(*avlt.search(4) == "hihi");
+  assert(*avlt.search(3) == "hehe");
+  assert(*avlt.search(2) == "haha");
   assert(*avlt.search(1) == "eita");
-  assert(*avlt.search(2) == "haha");
-  assert(*avlt.search(3) == "eita");
-  assert(*avlt.search(4) == "dorime");
-  assert(*avlt.search(5) == "ameno");
-  
-  avlt.remove(1);
-  
-  assert(avlt.search(1) == std::nullopt);
-  assert(*avlt.search(2) == "haha");
-  assert(*avlt.search(3) == "eita");
-  assert(*avlt.search(4) == "dorime");
-  assert(*avlt.search(5) == "ameno");
+}
 
-  return 0;
+void test3(){
+  #ifdef debug
+  std::cout << "test 3" << std::endl;
+  #endif
+  AVLTree<int, std::string> avlt{};
+
+  avlt.insert(3, "eita");
+  avlt.insert(2, "haha");
+  avlt.insert(4, "hehe");
+  avlt.insert(1, "hihi");
+
+  assert(*avlt.search(4) == "hehe");
+  assert(*avlt.search(3) == "eita");
+  assert(*avlt.search(2) == "haha");
+  assert(*avlt.search(1) == "hihi");
+
+  avlt.remove(4);
+
+  assert(avlt.search(4) == std::nullopt);
+  assert(*avlt.search(3) == "eita");
+  assert(*avlt.search(2) == "haha");
+  assert(*avlt.search(1) == "hihi");
+}
+
+int main(){
+  test1();
+  test2();
+  test3();
 }
