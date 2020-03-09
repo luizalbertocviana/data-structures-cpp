adb6c36aaa796057b24ce261757cc3e938bf6675
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-09 17:20:21 -0300

    first version of rbtree_test.cpp

---
+++ rbtree_test.cpp
@@ -0,0 +1,16 @@
+// assert macro
+#include <cassert>
+
+// we are going to test this data structure
+#include <rbtree.hpp>
+
+int main(){
+  RBTree<int, std::string> rbtree{};
+  RBTree<int, std::string> rbtree2{1, "eita"};
+  
+  rbtree.insert(1, "eita");
+
+  assert(*rbtree.search(1) == "eita");
+  
+  return 0;
+}
