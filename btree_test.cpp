5eceb7cf7d76a91941df863aa03973ff51d1726c
Luiz Alberto <luizalbertocviana@gmail.com>
2020-03-12 12:44:25 -0300

    added some tests

--- btree_test.cpp
+++ btree_test.cpp
@@ -1,11 +1,35 @@
 #include <cassert>
 
+// #define debug
+
 #include <btree.hpp>
 
+#ifdef debug
+#include <iostream>
+#endif
+
 int main(){
-  BTree<int, int, 3> btree{};
+  BTree<int, std::string, 3> btree{};
+  #ifdef debug
+  std::cout << "created empty tree" << std::endl;
+  #endif
 
   assert(btree.isEmpty());
+
+  std::string letter{"a"};
+  for (int i = 0; i < 26; i++){
+    btree.insert(i, letter);
+    letter[0]++;
+    #ifdef debug
+    std::cout << "inserted key " << i << std::endl;
+    #endif
+  }
+
+  letter[0] = 'a';
+  for ( int i = 0; i < 26; i++){
+    assert(*btree.search(i) == letter);
+    letter[0]++;
+  }
   
   return 0;
 }
