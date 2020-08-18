9d7abbb9f3680686f3fdddce6d52f286a17b930a
Luiz Alberto <luizalbertocviana@gmail.com>
2020-08-18 14:38:28 -0300

    refactoring of Matrix_test.cpp

--- Matrix_test.cpp
+++ Matrix_test.cpp
@@ -2,7 +2,7 @@
 
 #include <Matrix.hpp>
 
-int main(){
+void test_matrix(){
   Matrix<bool> m {3, 4};
 
   m.at(0,0) = false;
@@ -10,6 +10,46 @@ int main(){
 
   assert(m.at(0,0) == false);
   assert(m.at(1,1) == true);
+}
+
+void test_square_matrix(){
+  SquareMatrix<bool> m {4};
+
+  m = true;
+
+  m.at(0,0) = false;
+  m.at(3,2) = false;
+
+  assert(m.at(0,0) == false);
+  assert(m.at(3,2) == false);
+  assert(m.at(3,3) == true);
+}
+
+void test_upper_triangular_matrix(){
+  UpperTriangularMatrix<bool> ut_m {4};
+
+  ut_m.at(3,3) = false;
+
+  ut_m.at(1,0) = true;
+
+  assert(ut_m.at(1,0) == false);
+  assert(ut_m.at(0,1) == false);
+
+  ut_m.at(0, 1) = true;
+
+  assert(ut_m.at(0,1) == true);
+
+  ut_m.at(2,3) = true;
+
+  assert(ut_m.at(2,3) == true);
+}
+
+int main(){
+  test_matrix();
+
+  test_square_matrix();
+
+  test_upper_triangular_matrix();
 
   return 0;
 }
