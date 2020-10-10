175e6c55c20035529d3db791df2e5ce5271dc090
Luiz Alberto <luizalbertocviana@gmail.com>
2020-10-10 16:57:26 -0300

    added more tests to WeightedGraph

--- WeightedGraph_test.cpp
+++ WeightedGraph_test.cpp
@@ -2,7 +2,7 @@
 
 #include <WeightedGraph.hpp>
 
-int main(){
+void test1(){
   WeightedGraph wg {10};
   assert(wg.num_verts == 10);
   assert(wg.num_edges == 0);
@@ -38,6 +38,40 @@ int main(){
 
   wg.set_edge_weight(1, 2, 100);
   assert(wg.edge_weight(1, 2) == 0);
+}
+
+void test2(){
+  WeightedGraph wg {6};
+  assert(wg.num_verts == 6);
+  assert(wg.num_edges == 0);
+
+  wg.add_edge(0, 1, 11);
+  assert(wg.num_edges == 1);
+  wg.add_edge(0, 2, 11);
+  assert(wg.num_edges == 2);
+  wg.add_edge(0, 3, 8);
+  assert(wg.num_edges == 3);
+  wg.add_edge(0, 4, 3);
+  assert(wg.num_edges == 4);
+  wg.add_edge(1, 3, 1);
+  assert(wg.num_edges == 5);
+  wg.add_edge(1, 5, 42);
+  assert(wg.num_edges == 6);
+  wg.add_edge(2, 3, 7);
+  assert(wg.num_edges == 7);
+  assert(wg.has_edge(2, 4) == false);
+  wg.add_edge(2, 4, 1);
+  assert(wg.has_edge(2, 4) == true);
+  assert(wg.num_edges == 8);
+  assert(wg.has_edge(3, 5) == false);
+  wg.add_edge(3, 5, 5);
+  assert(wg.has_edge(3, 5) == true);
+  assert(wg.num_edges == 9);
+}
+
+int main(){
+  test1();
+  test2();
 
   return 0;
 }
